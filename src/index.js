699e290fda5aa8461ac7d671ef6e21fe9a620f03
dlh1201 <87434065+dlh9166@users.noreply.github.com>
2023-04-13 19:28:06 +0700

    install husky

--- src/index.js
+++ src/index.js
@@ -1,45 +1,59 @@
+const express = require('express');
+const app = express();
+const port = 3131;
+const path = require('path');
+const route = require('./routes/index');
 
-const express = require('express')
-const app = express()
-const port = 3131
-const morgan = require('morgan')
-const path = require('path')
-
+const morgan = require('morgan');
 //logo, file tĩnh thi no se vao public/...
 app.use(express.static(path.join(__dirname, 'public')));
 
+//middleware, dang form html
+app.use(
+    express.urlencoded({
+        extended: true,
+    })
+);
+app.use(express.json()); //dang gui tu js
 
 //handlebars
-const handlebars = require('express-handlebars')
+const handlebars = require('express-handlebars');
 // import { engine } from 'express-handlebars';
 
-
-
 //httplogger
-app.use(morgan('combined'))
+// app.use(morgan('combined'))
+
+//đường dẫn
 // console.log('path::: ', path.join(__dirname, 'resource/views'));
 
 //template engine
-app.engine('hbs', handlebars.engine(
-  {extname: '.hbs'}
-));
+app.engine('hbs', handlebars.engine({ extname: '.hbs' }));
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'resource/views'));
 
-;
-
-
-app.get('/', (req, res) => {
-  res.render('home')
-})
-
-
-
-app.get('/news', (req, res) => {
-  res.render('news')
-})
-
+route(app);
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+    console.log(`Example app listening on port ${port}`);
+});
+
+//khi chua phan nhanh
+// app.get('/', (req, res) => {
+//   res.render('home')
+// })
+
+// app.get('/news', (req, res) => {
+//   console.log(req.query.q)
+//   res.render('news')
+// })
+
+// app.use('/news', newsRouter)
+
+// app.get('/search', (req, res) => {
+//   res.render('search')
+// })
+
+// app.post('/search', (req, res) => {
+//   console.log(req.body)
+//   res.send('')
+// })
